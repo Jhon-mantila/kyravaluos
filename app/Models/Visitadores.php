c732a1bd7898b39e74a2802a649276ece8e77a8a
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2025-02-02 14:07:14 -0500

    Visitadores, factorym tabla, seeder

---
+++ app/Models/Visitadores.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Visitadores extends Model
+{
+    //
+    use HasFactory;
+    use HasUuids; // para visualizar bien los id creados con uuid
+
+    protected $fillable = [
+        'id',
+        'cobertura',
+        'ciudad',
+        'departamento',
+        'active',
+        'user_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/VisitadoresFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Uuid;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Visitadores>
+ */
+class VisitadoresFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'id' =>  (string) Uuid::uuid4(),
+            'cobertura' => $this->faker->word, // Genera una palabra aleatoria
+            'ciudad' => $this->faker->city, // Genera una ciudad aleatoria
+            'departamento' => $this->faker->state, // Genera un departamento/estado aleatorio
+            'active' => $this->faker->boolean, // Genera un valor booleano (true o false)
+            'user_id' => 1, //\App\Models\User::factory(), // Relación con el modelo User, pero si se crean 10 visitadores realaciona 10 usauruos y los crea
+            
+        ];
+    }
+}
